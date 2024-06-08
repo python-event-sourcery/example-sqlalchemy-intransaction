2657a59a93bcec34472e32ae77b84362193d86b4
Łukasz Żukowski <lukasz.zukowski@gmail.com>
2024-06-08 10:52:10 +0200

    Inventory: create_item

--- inventory.py
+++ inventory.py
@@ -1,4 +1,38 @@
-from fastapi import APIRouter, Body
+from dataclasses import dataclass
+from typing import ClassVar, Generator
+
+from event_sourcery.aggregate import Aggregate, Repository
+from event_sourcery.event_store import Backend, Event, StreamUUID
+from fastapi import APIRouter, Body, Depends
+
+from backend import backend
+
+
+@dataclass
+class Quantity(Aggregate):
+    category: ClassVar[str] = "quantity"
+    quantity: int = 0
+
+    class Adjust(Event):
+        quantity: int
+
+    def adjust(self, by: int) -> None:
+        self._emit(self.Adjust(quantity=self.quantity + by))
+
+    def __apply__(self, event: Event) -> None:
+        assert isinstance(event, Quantity.Adjust)
+        self.quantity = event.quantity
+
+
+class QuantityRepository(Repository[Quantity]):
+    pass
+
+
+def quantity_repository(
+    backend: Backend = Depends(backend),
+) -> Generator[QuantityRepository, None, None]:
+    yield QuantityRepository(backend.event_store)
+
 
 router = APIRouter(
     prefix="/inventory",
@@ -14,8 +48,10 @@ router = APIRouter(
 def create_item(
     item: str = Body(...),
     quantity: int = Body(...),
+    repository: QuantityRepository = Depends(quantity_repository),
 ) -> None:
-    raise NotImplementedError
+    with repository.aggregate(StreamUUID(name=item), Quantity()) as aggregate:
+        aggregate.adjust(by=quantity)
 
 
 @router.get(
